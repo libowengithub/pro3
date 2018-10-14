c108822b0b2816ebcb38ac0e0e60d1280e0dd442
libowen <libowen@qq.com>
2018-10-14 18:58:32 +0800

    本地工程添加添加

--- src/com/itheima/git_test.java
+++ src/com/itheima/git_test.java
@@ -4,4 +4,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("git_test");
     }
+    public void test(){
+        System.out.println("加精啊");
+    }
 }
